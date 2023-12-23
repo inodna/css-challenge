98ef942000f933e9a7ba1eb63e0b704c0fcf12a7
Andoni <and.w3w@gmail.com>
2023-12-23 12:08:44 +0100

    Add className prop to Logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,5 +1,13 @@
 import SiteLogo from "../../assets/logo.svg?react";
 
-export const Logo = () => {
-  return <SiteLogo height={48} />;
+interface SiteLogoProps {
+  className?: string;
+}
+
+export const Logo = ({ className }: SiteLogoProps) => {
+  return (
+    <a href="/" className={className}>
+      <SiteLogo height={48} />
+    </a>
+  );
 };
