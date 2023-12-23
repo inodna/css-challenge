0e19be0f399e044838c7ea1f6b0a130660f08eed
Andoni <and.w3w@gmail.com>
2023-12-23 16:24:41 +0100

    Use correct heading hierarchy

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,13 +1,18 @@
+import { classNames } from "../../utils/classNames";
 import styles from "./Heading.module.scss";
 
 interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement> {
   level: "h1" | "h2" | "h3" | "h4" | "h5";
 }
 
-const Heading: React.FC<HeadingProps> = ({ level, children }) => {
+const Heading: React.FC<HeadingProps> = ({ level, children, className }) => {
   const Heading = level;
 
-  return <Heading className={styles[level]}>{children}</Heading>;
+  return (
+    <Heading className={classNames(styles[level], className!)}>
+      {children}
+    </Heading>
+  );
 };
 
 export default Heading;
